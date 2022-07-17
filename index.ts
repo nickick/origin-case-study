615848e687551528c3fe1e8f15f910386c3d3d43
nickick <hong.nick@gmail.com>
2022-07-17 13:34:16 -0400

    Add ratio of counts to each trait

--- index.ts
+++ index.ts
@@ -1,23 +1,43 @@
 import PudgyPenguins from './PudgyPenguin.json';
+const penguinCount = PudgyPenguins.length;
 
-const attributesMap = {};
-
-// set up a mapping of attributes across PudgyPenguins, getting total counts
-PudgyPenguins.forEach(({attributes}) => {
-  attributes.forEach(({trait_type, value}) => {
-    // if first time this attribute trait type is found, set it with a value count of 1 for this value
-    if (!attributesMap[trait_type]) {
-      attributesMap[trait_type] = {
-        [value]: 1,
+function countAttributes () {
+  const attributesMap = {};
+  
+  // set up a mapping of attributes across PudgyPenguins, getting total counts
+  PudgyPenguins.forEach(({attributes}) => {
+    attributes.forEach(({trait_type, value}) => {
+      // if first time this attribute trait type is found, set it with a value count of 1 for this value
+      if (!attributesMap[trait_type]) {
+        attributesMap[trait_type] = {
+          [value]: {
+            count: 1,
+          }
+        }
+      // or of this attribute trait type and value, then set this value and type
+      } else if (!attributesMap[trait_type][value]) {
+        attributesMap[trait_type][value] = {
+          count: 1,
+        }
+      // otherwise increment count by 1
+      } else {
+        const newCount = attributesMap[trait_type][value].count + 1;
+        attributesMap[trait_type][value].count = newCount;
       }
-    // or of this attribute trait type and value, then set this value and type
-    } else if (!attributesMap[trait_type][value]) {
-      attributesMap[trait_type][value] = 1;
-    // otherwise increment count by 1
-    } else {
-      attributesMap[trait_type][value] += 1;
-    }
+    })
   })
-})
 
-console.log(attributesMap)
+  return attributesMap;
+}
+
+function setRatio (mapping) {
+  for (const trait_type in mapping) {
+    for (const trait in mapping[trait_type]) {
+      mapping[trait_type][trait].ratio = mapping[trait_type][trait].count / penguinCount;
+    }
+  }
+
+  return mapping;
+}
+
+console.log(setRatio(countAttributes()))
